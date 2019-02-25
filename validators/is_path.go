e07800542fb32a275163c40862562b4f7046026d
s3rj1k <evasive.gyron@gmail.com>
2019-02-25 16:39:41 +0200

    add is_path validator
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

---
+++ validators/is_path.go
@@ -0,0 +1,30 @@
+package validators
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/s3rj1k/validator"
+)
+
+// IsPath is a validator object
+type IsPath struct {
+	Name    string
+	Path    string
+	Message string
+}
+
+// Validate adds an error if the path does not exist.
+func (v *IsPath) Validate(e *validator.Errors) {
+
+	if _, err := os.Stat(v.Path); !os.IsNotExist(err) {
+		return
+	}
+
+	if len(v.Message) > 0 {
+		e.Add(v.Name, v.Message)
+		return
+	}
+
+	e.Add(v.Name, fmt.Sprintf("path '%s' must exist", v.Path))
+}
---
+++ validators/is_path_test.go
@@ -0,0 +1,52 @@
+package validators
+
+import (
+	"os"
+	"testing"
+
+	"github.com/s3rj1k/validator"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_IsNoPath(t *testing.T) {
+
+	r := require.New(t)
+
+	fd, err := os.Create("/tmp/test")
+	r.Nil(err)
+
+	err = fd.Close()
+	r.Nil(err)
+
+	defer func(r *require.Assertions) {
+		err = os.Remove("/tmp/test")
+		r.Nil(err)
+	}(r)
+
+	v := IsNoPath{Name: "Name", Path: "/tmp/doesnotexist"}
+	e := validator.NewErrors()
+	v.Validate(e)
+
+	r.Equal(0, e.Count())
+
+	v = IsNoPath{Name: "Name", Path: "/tmp/test"}
+	v.Validate(e)
+
+	r.Equal(1, e.Count())
+	r.Equal([]string{"path '/tmp/test' must not exist"}, e.Get("Name"))
+
+	e = validator.NewErrors()
+	v = IsNoPath{Name: "Name", Path: "/tmp/test", Message: "path must not exist"}
+	v.Validate(e)
+
+	r.Equal(1, e.Count())
+	r.Equal([]string{"path must not exist"}, e.Get("Name"))
+
+	e = validator.NewErrors()
+	v = IsNoPath{"Name", "/tmp/test", "path must not exist"}
+	v.Validate(e)
+
+	r.Equal(1, e.Count())
+	r.Equal([]string{"path must not exist"}, e.Get("Name"))
+}
